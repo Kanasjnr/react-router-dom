981041d7df4248eb22df971efefe7f2b683d4a16
Kanasjnr <141998286+Kanasjnr@users.noreply.github.com>
2023-11-02 12:09:22 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,8 @@ const App = () => {
   const [searchResult, setSearchResult] = useState([]);
   const [postTitle, setPostTitle] = useState("");
   const [postBody, setPostBody] = useState("");
+  const [editTitle, setEditTitle] = useState("");
+  const [editBody, setEditBody] = useState("");
 
   useEffect(() => {
     const filterResult = posts.filter(
@@ -47,7 +49,7 @@ const App = () => {
   const navigate = useNavigate();
 
   const handleDelete = async (id) => {
-
+    ;
     try {
       await api.delete(`/posts/${id}`);
       const postLists = posts.filter((post) => post.id !== id);
@@ -60,65 +62,71 @@ const App = () => {
   };
 
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
+    const date = format(new Date(), "MMMM dd, yyyy pp");
+    const newPost = { id, title: postTitle, date, body: postBody };
+    try {
+      const response = await api.post('/posts', newPost)
+      const allPost = [...posts, response.data];
+      setPosts(allPost);
+      setPostTitle("");
+      setPostBody("");
+      navigate("/");
+    } catch (error) {
+      console.log(`Error: ${error.message}`);
+    }
+
+  };
 
 
-
-const handleSubmit = async (e) => {
-  e.preventDefault();
-  const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
-  const date = format(new Date(), "MMMM dd, yyyy pp");
-  const newPost = { id, title: postTitle, date, body: postBody };
-  try {
-    const response = await api.post('/posts', newPost)
-    const allPost = [...posts, response.data];
-    setPosts(allPost);
-    setPostTitle("");
-    setPostBody("");
-    navigate("/");
-  } catch (error) {
-    console.log(`Error: ${error.message}`);
+  const handleEdit = async (id) => {
+    const date = format(new Date(), "MMMM dd, yyyy pp");
+    const updatedPost = { id, title: postTitle, date, body: editBody };
+    try {
+      const response = await api.put(`/posts/${id}`, updatedPost)
+      setPosts(posts.map(post => post.id === id ? { ...response.data } : post))
+      setEditTitle('')
+      setEditBody('')
+      navigate('/')
+    } catch (error) {
+      console.log(`Error: ${error.message}`);
+    }
   }
 
-};
 
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={<HomeLayout search={search} setSearch={setSearch} />}
+      >
+        <Route index element={<Home posts={searchResult} />} />
+        <Route path="/post">
+          <Route
+            index
+            element={
+              <NewPost
+                postTitle={postTitle}
+                setPostTitle={setPostTitle}
+                postBody={postBody}
+                setPostBody={setPostBody}
+                handleSubmit={handleSubmit}
+              />
+            }
+          />
 
-const handleEdit = async(id) => {
-  const date = format(new Date(), "MMMM dd, yyyy pp");
-  const newPost = { id, title: postTitle, date, body: postBody };
-}
-
-
-return (
-  <Routes>
-    <Route
-      path="/"
-      element={<HomeLayout search={search} setSearch={setSearch} />}
-    >
-      <Route index element={<Home posts={searchResult} />} />
-      <Route path="/post">
-        <Route
-          index
-          element={
-            <NewPost
-              postTitle={postTitle}
-              setPostTitle={setPostTitle}
-              postBody={postBody}
-              setPostBody={setPostBody}
-              handleSubmit={handleSubmit}
-            />
-          }
-        />
-
-        <Route
-          path=":id"
-          element={<PostPage posts={posts} handleDelete={handleDelete} />}
-        />
+          <Route
+            path=":id"
+            element={<PostPage posts={posts} handleDelete={handleDelete} />}
+          />
+        </Route>
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<Missing />} />
       </Route>
-      <Route path="/about" element={<About />} />
-      <Route path="*" element={<Missing />} />
-    </Route>
-  </Routes>
-);
+    </Routes>
+  );
 };
 
 export default App;
