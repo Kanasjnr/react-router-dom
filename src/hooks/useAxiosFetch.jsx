e1eb86a39a30944fe491a8c331138c7b6cad1425
Kanasjnr <141998286+Kanasjnr@users.noreply.github.com>
2023-11-03 11:35:06 +0100

    update

--- src/hooks/useAxiosFetch.jsx
+++ src/hooks/useAxiosFetch.jsx
@@ -5,8 +5,24 @@ import axios from 'axios'
 import React from 'react'
 
 const useAxiosFetch = (dataUrl) => {
-    const [data,setData] = useState([])
-   
+    const [data, setData] = useState([])
+    const [fetchError, setFetchError] = useState(null)
+    const [isLoading, setIsLoading] = useState(false)
+
+    useEffect(() => {
+        let isMounted = true
+        const source = axios.CancelToken.source()
+
+        const fetchData = async (url) =>{
+            setIsLoading(true)
+            try {
+                
+            } catch (error) {
+                
+            }
+        }
+
+    }, [])
     return (
         <>
 
